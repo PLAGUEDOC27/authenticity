0666036e6570e7867aeb9ad53eca831d8097247b
tytus <23ucs026@loyolacollege.edu>
2026-03-24 09:15:08 +0530

    change

--- utils/plagiarism_engine.py
+++ utils/plagiarism_engine.py
@@ -1,12 +1,11 @@
-import difflib
 import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 def split_sentences(text):
-    return re.split(r'[.!?]', text)
-
-
-def similarity(a, b):
-    return difflib.SequenceMatcher(None, a, b).ratio()
+    sentences = re.split(r'[.!?]', text)
+    return [s.strip() for s in sentences if len(s.strip()) > 10]
 
 
 def check_plagiarism(new_text, documents):
@@ -14,7 +13,8 @@ def check_plagiarism(new_text, documents):
     new_sentences = split_sentences(new_text)
 
     report = []
-    highest_score = 0
+    total_score = 0
+    match_count = 0
 
     for doc in documents:
 
@@ -26,19 +26,30 @@ def check_plagiarism(new_text, documents):
         for s1 in new_sentences:
             for s2 in existing_sentences:
 
-                score = similarity(s1.strip(), s2.strip())
+                # TF-IDF Vectorization (2 sentences at a time)
+                vectorizer = TfidfVectorizer(ngram_range=(1, 2))  # unigram + bigram
 
-                if score > 0.75:   # similarity threshold
+                vectors = vectorizer.fit_transform([s1, s2])
+
+                similarity_matrix = cosine_similarity(vectors[0:1], vectors[1:2])
+                score = similarity_matrix[0][0]
+
+                if score > 0.3:  # threshold (IMPORTANT)
 
                     percent = round(score * 100, 2)
 
                     report.append({
-                        "matched_sentence": s1.strip(),
+                        "matched_sentence": s1,
                         "source_document": doc.filename,
                         "similarity": percent
                     })
 
-                    if percent > highest_score:
-                        highest_score = percent
+                    total_score += percent
+                    match_count += 1
 
-    return highest_score, report
+    if match_count > 0:
+        final_score = total_score / match_count
+    else:
+        final_score = 0
+
+    return round(final_score, 2), report
